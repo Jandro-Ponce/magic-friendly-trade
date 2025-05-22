2f7ad314aa448098a7dc0df391d9e1ef942f4d03
Jandro <jandroponpal@gmail.com>
2025-05-22 08:49:21 +0200

    first commit

---
+++ src/user/controller/user.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Post, Get, Body } from '@nestjs/common';
+import { UserService } from '../service/user.service';
+import { CreateUserDto } from '../dto/create-user.dto';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
+}
