4447cdcd3e1fb325e29cfb72c594a07cfdeb496a
Jandro <jandroponpal@gmail.com>
2025-05-22 13:22:29 +0200

    refactor de user repository para mejor entendimiento

--- src/user/repository/user.repository.ts
+++ src/user/repository/user.repository.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from '../entity/user.entity';
+import { CreateUserDto } from '../dto/create-user.dto';
 
 @Injectable()
 export class UserRepository {
@@ -11,17 +12,20 @@ export class UserRepository {
   ) {}
 
   async findAll(): Promise<User[]> {
+
     return this.repository.find();
   }
 
   async findById(id: number): Promise<User | null> {
+
     return this.repository.findOneBy({ id });
   }
 
-  async createAndSave(user: Partial<User>): Promise<User> {
-    const newUser = this.repository.create(user);
-    return this.repository.save(newUser);
-  }
+  async createAndSave(userData: CreateUserDto): Promise<User> {
+  const newUser = this.repository.create(userData);
+  
+  return this.repository.save(newUser);
+}
 
   async remove(id: number): Promise<void> {
     await this.repository.delete(id);
