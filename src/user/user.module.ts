f8262093a86bdc9186074cf9150148a458bcd092
Jandro <jandroponpal@gmail.com>
2025-05-26 14:38:30 +0200

    Agregado modulos para que nest comprenda que existen

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,11 +4,26 @@ import { User } from './entity/user.entity';
 import { UserService } from './service/user.service';
 import { UserController } from './controller/user.controller';
 import { UserRepository } from './repository/user.repository';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MailModule } from 'src/mail/mail.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+    }),
+    MailModule,
+  ],
   controllers: [UserController],
   providers: [UserService, UserRepository],
-  exports: [UserService],
+  exports: [UserService, UserRepository],
 })
 export class UserModule {}
