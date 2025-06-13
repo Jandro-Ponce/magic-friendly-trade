5b60b3a62d004cde405e0f7f82e817c2b1f45985
Jandro <jandroponpal@gmail.com>
2025-06-14 00:10:44 +0200

    Prueba front

--- apps/trade-web/src/App.tsx
+++ apps/trade-web/src/App.tsx
@@ -1,26 +1,30 @@
-import { useState } from 'react'
+import { useState, type PropsWithChildren } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
+function Logos(){
+  return <><a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </a></>
+}
+function App() {
+  const [username, setName] = useState("")
+
+  function onclickHandler(){
+    alert("Login with name: " + username)
+  }
+
+  return (
+    <>
+      <div>
+        <Logos />
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -28,8 +32,13 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <input type='text' placeholder='Username' value={username} onChange={(e)=>setName(e.target.value.toLowerCase())}></input>
+      <button onClick={ onclickHandler }>
+          Login
+      </button>
     </>
   )
 }
 
 export default App
+
