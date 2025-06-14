664871ee858b993c6b416e170c6a15ca13bbf387
Alejandro Ponce Palau <57868943+Zackfair200@users.noreply.github.com>
2025-06-14 02:02:52 +0200

    Add dark Material UI theme

--- apps/trade-web/src/main.tsx
+++ apps/trade-web/src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { CssBaseline } from '@mui/material'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { darkTheme } from './theme'
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <CssBaseline />
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
