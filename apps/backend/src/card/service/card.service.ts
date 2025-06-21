f8bc3753956bc8ace6fe07771c891c9853ffa47f
Jandro <jandroponpal@gmail.com>
2025-06-22 00:54:38 +0200

    agregado multilenguaje

--- apps/backend/src/card/service/card.service.ts
+++ apps/backend/src/card/service/card.service.ts
@@ -5,11 +5,12 @@ export class CardService {
   private readonly apiUrl = 'https://api.scryfall.com';
 
   async search(query: string): Promise<any> {
-    const response = await fetch(
-      `${this.apiUrl}/cards/search?q=${encodeURIComponent(query)}`,
-    );
+    const requesturl = `${this.apiUrl}/cards/search?q=${encodeURIComponent(query)}&include_multilingual=true`;
+    const response = await fetch(requesturl);
 
     if (!response.ok) {
+      console.log('URL:', requesturl);
+      console.log('Error fetching cards:', response);
       throw new Error('Failed to fetch cards');
     }
 
